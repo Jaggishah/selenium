1d1829873171a3effa89b8a8e531ea82f04b08ef
Jaggishah <jugdeepshah@gmail.com>
2022-10-10 01:01:48 -0700

    explicit read

--- waitingcommands2.py
+++ waitingcommands2.py
@@ -12,3 +12,8 @@ link.submit()
 driver.find_element(By.XPATH,"//h3[text()='Selenium']").click()
 searchlink = mywait.until(ec.presence_of_element_located((By.XPATH,"//h3[text()='Selenium']")))
 searchlink.click()
+
+# explicit just wait for some time and then take 10 argument which is specified above and wait for 10 second and go on
+# if element find is good if not exceptions raised 
+# explicit is done by object created through webdriverwait
+# then above 13th lines
