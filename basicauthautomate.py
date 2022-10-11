61b951e7d16db32b9cddd45a18eae1b77288aeda
Jaggishah <jugdeepshah@gmail.com>
2022-10-11 00:26:52 -0700

    basic auth popup

---
+++ basicauthautomate.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth")
